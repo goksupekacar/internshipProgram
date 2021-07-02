83724e45357a8d1c87e42a6c7d8f631f4ca4017c
Göksu <goksu.pekacar@hotmail.com>
2021-07-02 14:44:01 +0300

    Bug fix

--- intern_core/forms.py
+++ intern_core/forms.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class RegisterFormIntern(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
